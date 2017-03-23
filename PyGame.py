765be338ba9f873fe6ad8f9632d6a6fb503eb0c3
dylanmitchell1007 <armadummy@yahoo.com>
2017-03-23 11:00:48 -0500

    b

---
+++ PyGame.py
@@ -0,0 +1,11 @@
+A*(start, goal)
+closedSet := {}
+openSet := {}
+cameFrom := the empty map
+gScore := map with default value of Infinity
+gScore[Start]:= 0
+fScore := map with default value of Infinity
+fScore[start] := heuristic_cost_estimate(start, goal)
+
+while openSet is not empty:
+    current := 
